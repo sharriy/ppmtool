efc6294003da26cf9e63564ed7b09a495c19fc28
Riya Sharma <sharmariya8497@gmail.com>
2021-03-13 04:16:15 +0530

    Frontend CRUD operations

--- backend/src/main/java/com/restfulwebservices/ppmtool/domain/Project.java
+++ backend/src/main/java/com/restfulwebservices/ppmtool/domain/Project.java
@@ -3,6 +3,8 @@ package com.restfulwebservices.ppmtool.domain;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,7 +17,7 @@ import javax.validation.constraints.Size;
 
 @Entity 
 public class Project {
-
+ 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -23,7 +25,7 @@ public class Project {
 	private String projectName;
 	@NotBlank(message="Project Identifier is required")
 	@Size(min=4, max=5, message="Please use 4 to 5 characters")
-	@javax.persistence.Column(updatable = false, unique = true)
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
 	@NotBlank(message="Project description is required")
 	private String description;
